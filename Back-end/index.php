ff11611f9432c7ebca0c73742b998869ef6b44f6
aditya das <adas@cs.odu.edu>
2017-07-28 19:40:20 -0400

    initial message

---
+++ Back-end/index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="code.js"></script>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <br />
+    <br />
+    <a href="buy.php" class="button">&nbsp;Buy Cars &nbsp;</a><br /><br />
+    <a href="sell.php" class="button">&nbsp;Sell Cars &nbsp;</a><br /><br />
+  </body>
+</html>
